e140fd7594f6160b5873add5e5a5e3cd964659ed
José Sabater Montes <jsm@iaa.es>
2013-05-30 22:34:05 +0100

    Try to use the builtin copy of wcs in precise64. Configuration file for monetdb added

--- vagrant_precise64/bootstrap.sh
+++ vagrant_precise64/bootstrap.sh
@@ -17,17 +17,7 @@ libcfitsio3-dev libxml2-dev libpng-dev libblas-dev liblapack-dev bison libboost-
 f2c zlib1g-dev subversion libfreetype6-dev make libncurses5-dev git
 
 ## wcs library
-cd
-wget ftp://ftp.atnf.csiro.au/pub/software/wcslib/wcslib.tar.bz2
-tar xfj wcslib.tar.bz2
-cd wcslib-4.17
-./configure
-make
-mkdir -p /usr/local/share/man/man1
-make install
-cd ..
-#rm -rf wcslib-4.17
-#rm wcslib.tar.bz2
+apt-get install -y wcslib-dev
 
 ## hdf5
 #apt-get install -y libhdf5-openmpi-dev 
