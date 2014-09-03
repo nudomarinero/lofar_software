2dc5c1d36526c502f9c608bef91f855a2cd5994c
José Sabater Montes <jsm@iaa.es>
2014-09-03 16:04:38 +0200

    Update installation of PPA packages

--- vagrant_trusty64/bootstrap.sh
+++ vagrant_trusty64/bootstrap.sh
@@ -12,28 +12,55 @@ apt-get upgrade -y
 apt-get install -y gfortran cmake scons fftw3-dev flex libreadline-dev libcfitsio3 \
 libcfitsio3-dev libxml2-dev libpng-dev libblas-dev liblapack-dev bison libboost-all-dev \
 f2c zlib1g-dev subversion libfreetype6-dev make libncurses5-dev git libatlas-base-dev \
-wcslib-dev hdf5-tools libhdf5-dev libhdf5-serial-dev build-essential
+wcslib-dev hdf5-tools libhdf5-dev libhdf5-serial-dev build-essential libzmq-dev \
+liblzo2-dev
 
 ## Casacore
+apt-add-repository -y ppa:jsm-8/lofar-dependencies
+apt-get update
+apt-get install -y casacore libcasacore-dev
 
 
+# ## Install Python packages
+apt-get install -y ipython python-matplotlib python-matplotlib-data python-pip \
+python-pyfits python-numpy python-scipy python-virtualenv python-sphinx \
+python-pygments python-jinja2 python-nose python-tornado cython python-zmq \
+python-pywcs python-astropy python-numexpr python-tables python-pandas \
+ipython-notebook ipython-qtconsole
 
-# ## Install Python dependencies
-# apt-get install -y ipython python-matplotlib python-matplotlib-data python-pip \
-# python-pyfits python-numpy python-scipy python-virtualenv
+
+# # ## Install updated Python packages (optional)
+# apt-get build-dep -y python-matplotlib python-pyfits python-numpy python-scipy \
+# ipython 
 # 
-# ## Install updated Python packages
+# pip install --upgrade readline
+# pip install --upgrade jinja2
+# pip install --upgrade pygments
+# pip install --upgrade sphinx
+# pip install --upgrade nose
+# pip install --upgrade tornado
+# # Cython does not compile with 1 GB of RAM
+# pip install --upgrade cython
+# pip install --upgrade pyzmq 
 # pip install --upgrade numpy
 # pip install --upgrade scipy
 # pip install --upgrade pyfits
 # pip install --upgrade matplotlib
+# pip install --upgrade astropy
 # pip install --upgrade pywcs
+# pip install --upgrade numexpr
+# pip install --upgrade tables
+# pip install --upgrade pandas
+# pip install --upgrade ipython
 
 ## pyrap
+apt-get install -y python-pyrap
 
 ## casarest
+apt-get install -y casarest libcasarest-dev
 
 ## Casapy or casacore-data
+apt-get install -y casacore-data
 
 ## LOFAR
 
